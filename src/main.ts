0309a1d2e107edaeb323a37a7bfb0d5f5449037b
tutanck <tutanck@gmail.com>
2023-06-14 11:04:57 +0200

    fix survey report date format

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,12 @@
-import "./style.css";
-import typescriptLogo from "./typescript.svg";
-import viteLogo from "/vite.svg";
+import "./css/style.css";
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
     <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+      f
     </a>
     <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
+      j
     </a>
     <h1>Vite + TypeScript</h1>
     <div class="card">
