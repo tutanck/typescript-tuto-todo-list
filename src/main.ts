fc7b5321743fb37d6a1a79df8109d049879d7cd7
tutanck <tutanck@gmail.com>
2023-06-14 11:07:36 +0200

    fix survey report date format

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,1 @@
 import "./css/style.css";
-
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      f
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      j
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`;
